73e28fb6ca16d30f2a71851006b83f739f596be8
Nikita Shanin <shaninnik@gmail.com>
2020-02-11 16:27:36 -0500

    test build

--- webpack.config.js
+++ webpack.config.js
@@ -105,7 +105,7 @@ module.exports = (env, options) => {
       new HtmlWebpackPlugin({
         filename: "utopia/index.html",
         baseUrl: process.env.NODE_ENV == 'development'?'/':'./',
-        template: './utopia/index.html',
+        template: './utopia/template.html',
         chunks: ['utopia'],
         inject: false
       })
